37f444714b8a17f79cb10875127f85dc5c13568d
Gbrlbrgm <gabriel_bergamo@outlook.com>
2025-05-28 22:17:39 -0300

    feat: ensure user has clinic on doctors page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import React from "react";
 
-import { db } from "@/db";
-import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./_components/sign-out-button";
@@ -16,11 +13,8 @@ const DashboardPage = async () => {
   if (!session?.user) {
     redirect("/authentication");
   }
-  const clinics = await db.query.usersToClinicsTable.findMany({
-    where: eq(usersToClinicsTable.userId, session.user.id),
-  });
-  if (clinics.length === 0) {
-    redirect("/clinic-form");
+  if (!session.user.clinic) {
+    redirect("/doctors");
   }
   return (
     <div>
