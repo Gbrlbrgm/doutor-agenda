1cf3c91290a623ca1e432692e7918cd2c857c42d
Gbrlbrgm <gabriel_bergamo@outlook.com>
2025-06-02 17:24:26 -0300

    feat: add date picker component with nuqs integration

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,10 +1,18 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import React from "react";
 
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
 import { auth } from "@/lib/auth";
 
-import SignOutButton from "./_components/sign-out-button";
+import { DatePicker } from "./_components/date-picker";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
@@ -14,15 +22,25 @@ const DashboardPage = async () => {
     redirect("/authentication");
   }
   if (!session.user.clinic) {
-    redirect("/doctors");
+    redirect("/clinic-form");
   }
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <h1>{session?.user?.email}</h1>
-      <h1>{session?.user?.name}</h1>
-      <SignOutButton />
-    </div>
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Pacientes</PageTitle>
+          <PageDescription>
+            Gerencie os pacientes da sua clínica
+          </PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <DatePicker />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <></>
+      </PageContent>
+    </PageContainer>
   );
 };
 
